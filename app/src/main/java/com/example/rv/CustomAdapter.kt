5fc2fbde592086269f4e9481e4c95259338d057a
JaeHyeon <jaehyeon0825@gmail.com>
2021-11-26 09:55:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rv/CustomAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.rv
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+
+class CustomAdapter (private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+
+        val view  = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
+
+
+        val ItemsViewModel = mList[position]
+
+
+        // sets the image to the imageview from our itemHolder class
+        holder.rv_button.setImageResource(ItemsViewModel.image)
+
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = ItemsViewModel.text
+
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class ViewHolder(ItemView:View) : RecyclerView.ViewHolder(ItemView) {
+        val rv_button: ImageButton = itemView.findViewById(R.id.rv_button)
+        val textView: TextView = itemView.findViewById(R.id.textView)
+    }
+}
---
+++ app/src/main/java/com/example/rv/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.rv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val data = ArrayList<ItemsViewModel>()
+
+        data.apply {
+            add(ItemsViewModel(image = R.drawable.ic_launcher_background, text = "강타"))
+            add(ItemsViewModel(image = R.drawable.ic_launcher_background, text = "ㄴ"))
+            add(ItemsViewModel(image = R.drawable.ic_launcher_background, text = "ㅇ"))
+            add(ItemsViewModel(image = R.drawable.ic_launcher_background, text = "ㅁ"))
+
+        }
+
+        val adapter = CustomAdapter(data)
+
+        recyclerView.adapter = adapter
+    }
+}
